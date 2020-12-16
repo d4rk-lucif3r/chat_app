1bd74a3df1e9e1157993c33c701f4bd2ff1084db
Arsh Anwar <lucifer78908@gmail.com>
2020-12-16 22:17:02 +0530

    Implemented Stream Builder
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 import './screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Firebase.initializeApp();
     return MaterialApp(
       title: 'Chat App',
       theme: ThemeData(
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,35 +1,36 @@
-import 'package:flutter/material.dart';
+
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
-  //final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
         title: Text('Chat App'),
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This Works'),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/Irios9aQw4awIHAEe7Op/messages')
+            .snapshots(),
+        builder: (ctx, snapShot) =>
+            snapShot.connectionState == ConnectionState.waiting
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemCount: snapShot.data.docs.length,
+                    itemBuilder: (ctx, index) => Container(
+                      padding: EdgeInsets.all(8),
+                      child: Text(snapShot.data.docs[index]['text']),
+                    ),
+                  ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          Firebase.initializeApp();
-          FirebaseFirestore.instance
-              .collection('chats/Irios9aQw4awIHAEe7Op/messages')
-              .snapshots()
-              .listen((event) {
-            event.docs.forEach((element) {
-              print(element['text']);
-            });
-          });
-        },
+        onPressed: () {},
       ),
     );
   }
