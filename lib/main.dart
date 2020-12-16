4638ff1056ba117572cea482bc8bed0600f996c1
Arsh Anwar <lucifer78908@gmail.com>
2020-12-16 23:51:53 +0530

    Added Basic Authentication Screen
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import './screens/chat_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
+import './screens/auth_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
